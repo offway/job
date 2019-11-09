883dde877124c3fdb1bd8fdc8ff56afec15e6128
XiaZhengXin <xzx@xiazhengxin.name>
2019-11-09 18:58:38 +0800

    内存表KEY字段长度调整为500，以免内容太长。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- Server.php
+++ Server.php
@@ -14,7 +14,7 @@ $server->set(array(
 //创建内存表
 $table = new swoole_table(2048);//参数指定表格的最大行数
 $table->column("timerId", swoole_table::TYPE_INT, 10);
-$table->column("key", swoole_table::TYPE_STRING, 50);
+$table->column("key", swoole_table::TYPE_STRING, 500);
 $table->create();
 //创建调度进程,目前单worker进程模式下没必要使用，多开workers 进程是需要的，因为 clear() 不能用于清除其他进程的定时器，只作用于当前进程
 $process = new swoole_process(function (swoole_process $worker) {
